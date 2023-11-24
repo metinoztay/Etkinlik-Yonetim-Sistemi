e86a28c62b6a561ab73c031740c118892ee27137
Metin ÖZTAY <metinoztay@gmail.com>
2023-11-24 22:59:29 +0300

    Framework Güncellemesi & Giriş Sayfası Tasarımı

---
+++ Etkinlik-Yonetim-Sistemi/frmGiris.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Etkinlik_Yonetim_Sistemi
+{
+    partial class frmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGiris));
+            this.picSolGiris = new System.Windows.Forms.PictureBox();
+            this.picKullanciAdiGiris = new System.Windows.Forms.PictureBox();
+            this.picKullaniciAdiGirisSol = new System.Windows.Forms.PictureBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblKullaniciGirisi = new System.Windows.Forms.Label();
+            this.btnGiris = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picSolGiris)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKullanciAdiGiris)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKullaniciAdiGirisSol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSolGiris
+            // 
+            this.picSolGiris.Image = global::Etkinlik_Yonetim_Sistemi.Properties.Resources.orange_side;
+            resources.ApplyResources(this.picSolGiris, "picSolGiris");
+            this.picSolGiris.Name = "picSolGiris";
+            this.picSolGiris.TabStop = false;
+            // 
+            // picKullanciAdiGiris
+            // 
+            this.picKullanciAdiGiris.BackColor = System.Drawing.Color.WhiteSmoke;
+            resources.ApplyResources(this.picKullanciAdiGiris, "picKullanciAdiGiris");
+            this.picKullanciAdiGiris.Name = "picKullanciAdiGiris";
+            this.picKullanciAdiGiris.TabStop = false;
+            // 
+            // picKullaniciAdiGirisSol
+            // 
+            this.picKullaniciAdiGirisSol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            resources.ApplyResources(this.picKullaniciAdiGirisSol, "picKullaniciAdiGirisSol");
+            this.picKullaniciAdiGirisSol.Name = "picKullaniciAdiGirisSol";
+            this.picKullaniciAdiGirisSol.TabStop = false;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtKullaniciAdi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtKullaniciAdi, "txtKullaniciAdi");
+            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtSifre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtSifre, "txtSifre");
+            this.txtSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtSifre.Name = "txtSifre";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblKullaniciGirisi
+            // 
+            resources.ApplyResources(this.lblKullaniciGirisi, "lblKullaniciGirisi");
+            this.lblKullaniciGirisi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblKullaniciGirisi.Name = "lblKullaniciGirisi";
+            // 
+            // btnGiris
+            // 
+            resources.ApplyResources(this.btnGiris, "btnGiris");
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // frmGiris
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.lblKullaniciGirisi);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.picKullaniciAdiGirisSol);
+            this.Controls.Add(this.picKullanciAdiGiris);
+            this.Controls.Add(this.picSolGiris);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmGiris";
+            ((System.ComponentModel.ISupportInitialize)(this.picSolGiris)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKullanciAdiGiris)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKullaniciAdiGirisSol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSolGiris;
+        private System.Windows.Forms.PictureBox picKullanciAdiGiris;
+        private System.Windows.Forms.PictureBox picKullaniciAdiGirisSol;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblKullaniciGirisi;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
---
+++ Etkinlik-Yonetim-Sistemi/frmGiris.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etkinlik_Yonetim_Sistemi
+{
+    public partial class frmGiris : Form
+    {
+        public frmGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi = txtKullaniciAdi.Text;
+            string sifre = txtSifre.Text;
+
+            string connectionString = "Data Source=.;Initial Catalog=dbEtkinlikYonetimSistemi;User ID=your_username;Password=your_password;";
+
+            // SQL sorgusunu oluşturun (parametre kullanarak SQL Injection saldırılarından korunmak için)
+            string query = "SELECT COUNT(*) FROM tbl_Kullanicilar WHERE KullaniciAdi = @KullaniciAdi AND SifreHash = @SifreHash";
+        }
+    }
+}
